5232625e84b6f2f1f4a1176eb85de6f29892a1ce
David Zhang <straivers98@gmail.com>
2021-04-15 02:00:08 -0400

    Add MemoryOp type

--- source/shard/memory/tracker.d
+++ source/shard/memory/tracker.d
@@ -1,17 +1,86 @@
 module shard.memory.tracker;
 
-__gshared g_memory_tracker;
+import shard.collections.array : UnmanagedArray;
+import shard.handle_pool;
+import shard.memory.allocator : Allocator;
+import shard.os.time : OsClockApi, TimeStamp;
+import std.bitmanip : bitfields;
 
-struct MemoryTracker {
-    void record_allocate(string type_name, void[] memory) {
+enum tracked_allocator_handle_name = "shard_memory_tracker_handle";
+alias TrackedAllocatorId = Handle32!tracked_allocator_handle_name;
+
+final class MemoryTracker {
+    this(OsClockApi clock, Allocator sys_allocator) {
+        _clock = clock;
+        _sys_allocator = sys_allocator;
+        _sys_allocator_id = add_allocator(TrackedAllocatorId());
+    }
+
+    ~this() {
+    }
+
+    const TrackedAllocatorId sys_allocator_id() {
+        return _sys_allocator_id;
+    }
+
+    TrackedAllocatorId add_allocator(TrackedAllocatorId parent) {
 
     }
 
-    void record_deallocate(string type_name, void[] memory) {
+    void remove_allocator(TrackedAllocatorId allocator) {
 
     }
 
-    void record_reallocate(string type_name, void[] old_place, void[] new_place) {
+    void record_allocate(TrackedAllocatorId allocator, string type_name, void[] memory) {
 
     }
+
+    void record_deallocate(TrackedAllocatorId allocator, string type_name, void[] memory) {
+
+    }
+
+    void record_reallocate(TrackedAllocatorId allocator, string type_name, void[] old_place, void[] new_place) {
+
+    }
+
+private:
+    OsClockApi _clock;
+
+    Allocator _sys_allocator;
+    TrackedAllocatorId _sys_allocator_id;
+
+    size_t _free_list_length;
+    AllocatorInfo* _free_list;
+
+    // StringCache _string_cache;
+
+    void[512 * size_t.sizeof] _allocator_mem;
+    HandlePool!(AllocatorInfo*, tracked_allocator_handle_name, 512) _allocators;
+}
+
+private:
+
+struct MemoryOp {
+    enum Kind: ubyte {
+        None = 0,
+        Allocate = 1,
+        Deallocate = 2,
+        Reallocate = 3
+    }
+
+    TimeStamp time;
+    string type_name;
+    size_t memory1;
+    size_t memory2;
+
+    mixin(bitfields(
+        Kind, "kind", 2,
+        size_t, "size1", 31,
+        size_t, "size2", 31
+    ));
+}
+
+struct AllocatorInfo {
+    AllocatorInfo* next_free;
+    UnmanagedArray!MemoryOp ops;
 }
