1ce38ed95fe857d68d1aee144f3cee6015d082d2
David Zhang <straivers98@gmail.com>
2021-05-29 16:05:04 -0400

    update tester

--- tester/source/app.d
+++ tester/source/app.d
@@ -2,35 +2,43 @@ module app;
 
 import shard.hash : Hash32;
 import shard.memory.allocators.system : SystemAllocator;
-import shard.utils.map : UnmanagedHashMap32;
-import std.random : randomCover, uniform;
+import std.random : uniform;
 import std.range : iota, lockstep;
-import std.stdio : writeln;
+import std.stdio : writeln, writefln;
 import std.datetime.stopwatch : StopWatch;
-import std.array : array;
-import core.memory : GC;
+import std.traits : fullyQualifiedName;
+
+// import shard.utils.map : UnmanagedHashMap32;
+import shard.utils.map2;
 
 void main() {
-    writeln("Running tester");
-    GC.disable();
+    HashTable!(Hash32, size_t) map;
+    test(map);
+    // writefln("%x", cast(byte) -1);
+
+    // union T {
+    //     byte[4] b = [-1, -1, -1, -1];
+    //     int i;
+    // }
+    // writefln("%x", T().i);
+    // writefln("%x", uint.max);
+}
+
+void test(Map)(ref Map map) {
+    writeln("Running test on ", typeof(map).stringof);
+
+    SystemAllocator mem;
+
+    auto sw = StopWatch();
+    sw.start();
 
     auto gt = StopWatch();
     gt.start();
 
-	auto sw = StopWatch();
-	sw.start();
-
-    SystemAllocator mem;
-    auto map = UnmanagedHashMap32!uint(mem.allocator_api());
-
     alias Unit = void[0];
     Unit[Hash32] hashes;
-    while (hashes.length < 10_000_000)
+    while (hashes.length < 1_000_000)
         hashes[Hash32(uniform(0, uint.max))] = Unit.init;
-
-    auto values = new Hash32[](10_000_000);
-    foreach(i, hash; lockstep(iota(values.length), hashes.byKey))
-        values[i] = hash;
 
     assert(map.is_empty());
     assert(map.size() == 0);
@@ -39,33 +47,32 @@ void main() {
 	writeln("Init time: ", init_time);
 	sw.reset();
 
-    foreach (i, v; values) {
+    foreach (i, v; lockstep(iota(hashes.length), hashes.byKey)) {
         map.insert(v, cast(uint) i, mem.allocator_api());
-        assert(map.contains(v));
-        assert(map.get(v) == i);
-        assert(map.size() == i + 1);
+        // assert(map.contains(v));
+        // assert(*map.get(v) == i);
+        // assert(map.size() == i + 1);
     }
 
 	const insert_time = sw.peek().total!"nsecs";
 	writeln("Insert time: ", insert_time);
 	sw.stop();
 
-	sw.reset();
-	sw.start();
+	// sw.reset();
+	// sw.start();
 
-    foreach (v; values) {
-        map.remove(v, mem.allocator_api());
-        assert(!map.contains(v));
-    }
+    // foreach (v; hashes.byKey) {
+    //     map.remove(v, mem.allocator_api());
+    //     assert(!map.contains(v));
+    // }
 
-	const remove_time = sw.peek().total!"nsecs";
-	writeln("Remove time: ", remove_time);
-	sw.stop();
+	// const remove_time = sw.peek().total!"nsecs";
+	// writeln("Remove time: ", remove_time);
+	// sw.stop();
 
     const total_time = gt.peek().total!"msecs";
     writeln("Total time: ", total_time);
     gt.stop();
 
     assert(map.size() == 0);
-    GC.enable();
 }
